7be17ccdb47d31ba4b4c21c933f2090723bde6c3
Gauthier Gris <gauthier.gris@sia-partners.com>
2021-10-27 11:09:09 +0200

    save work

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+from database import Base, engine
+from routers import user
+
 
 app = FastAPI()
+
+Base.metadata.create_all(engine)
+
+app.include_router(user.router)
+
+
 
 @app.get("/")
 def index():
